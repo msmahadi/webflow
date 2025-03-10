ec3a7e3fca17431eeb05a64557d430fb6111caaa
MS Mahadi <144730100+msmahadi@users.noreply.github.com>
2025-03-10 15:04:33 +0600

    Update secure-test-1.js

--- api/secure-test-1.js
+++ api/secure-test-1.js
@@ -1,1 +1,17 @@
 
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.resolve("./test-1.js"); // মূল ফাইলের লোকেশন
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to load script" });
+      return;
+    }
+
+    res.setHeader("Content-Type", "application/javascript");
+    res.status(200).send(data);
+  });
+}
