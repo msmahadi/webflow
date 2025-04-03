a59eac3e785dbd6e7ff63ab30e0157f960c9ba11
MS Mahadi <144730100+msmahadi@users.noreply.github.com>
2025-04-03 17:43:25 +0600

    Update email-booking-form-7-reply-mail-xano.js

--- booking-form/email-booking-form-7-reply-mail-xano.js
+++ booking-form/email-booking-form-7-reply-mail-xano.js
@@ -1,1 +1,20 @@
+function sendEmailToGoogleAppsScript(data) {
+  const formID = document.getElementById('bookingForm');
 
+  // **নতুন যুক্ত করা: HTML attribute থেকে appScriptURL URL নিয়েছি**
+  const appScriptURL = formID.getAttribute('data-google-sheet-appscript-url');
+
+  // Xano API থেকে Date ও Time ডেটা ফেচ করা হচ্ছে
+  fetch(appScriptURL, {
+    method: 'POST',
+    mode: 'no-cors', // ✅ CORS সমস্যা এড়ানোর জন্য
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+    .then(() => {
+      // console.log("Email request sent.");
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+    });
+}
